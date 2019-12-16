272d33181631e5272eb51838114d86693bdfb5c9
katyakvitkaph <kvitka.katja@gmail.com>
2019-12-16 20:14:55 +0200

    Updates

---
+++ precache-manifest.642e4cb79f1afb7081db24231987ba66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829858f97c19ed2c0e23444f20e28eb1",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "06c17463cddc77de5eae",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.0f54525f.chunk.css"
+  },
+  {
+    "revision": "c4514f33741556bac66e",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.a46ec965.chunk.js"
+  },
+  {
+    "revision": "06c17463cddc77de5eae",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.5c7499cf.chunk.js"
+  },
+  {
+    "revision": "e59a76a5178abb5b8fec",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.147158bb.js"
+  }
+]);
